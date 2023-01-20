bfe729302f4924c866029d46dc3df3d92ea3ad65
dp14787 <dmpatel4773@gmail.com>
2023-01-20 12:31:31 +0100

    final changes

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ loss, acc = tmodel.evaluate(x_test, y_test)
 
 img_num = 1
 while (os.path.isfile(f'digits/digit{img_num}.png')):
-	img = cv2.imread(f'digits/digit{img_num}.png')[:,:,0]
-	img = np.invert(np.array([img]))
-	prediction = tmodel.predict(img)
-	print(f"This digit is probably a {np.argmax(prediction)}")
-	plt.imshow(img[0], cmap=plt.cm.binary)
-	plt.show()
-	img_num += 1
+	try:
+		img = cv2.imread(f'digits/digit{img_num}.png')[:,:,0]
+		img = np.invert(np.array([img]))
+		prediction = tmodel.predict(img)
+		print(f"This digit is probably a {np.argmax(prediction)}")
+		plt.imshow(img[0], cmap=plt.cm.binary)
+		plt.show()
+	except:
+		print(f"Error! Some error with image file digit{img_num}.png")
+	
+	finally:
+		img_num += 1
